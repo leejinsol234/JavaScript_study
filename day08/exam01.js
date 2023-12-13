d5b24018eaf436d5b46c3283ffacf3a0feab5fbc
leejinsol234 <desirable7@naver.com>
2023-12-13 19:45:34 +0900

    이벤트 처리기 등록과 타이머, HTML 요소를 동적으로 읽고 쓰기

---
+++ day08/exam01.js
@@ -0,0 +1,8 @@
+var a = [2,4,6];
+console.log("배열",a,"의 길이는",a.length,"입니다.");
+
+console.log("배열 " + a + " 의 길이는 " + a.length + " 입니다.");
+
+var name = "Tom";
+var height = 172.5;
+console.log("그의 이름은 %s이며 키는 %f cm 입니다.",name,height);
