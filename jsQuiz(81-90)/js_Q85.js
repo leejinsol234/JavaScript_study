d1f1f6276a9cebd2aa1a043f20e7eeb111ace155
leejinsol234 <desirable7@naver.com>
2024-02-01 17:45:59 +0900

    js 100제 81번~90번

--- jsQuiz(81-90)/js_Q85.js
+++ jsQuiz(81-90)/js_Q85.js
@@ -19,9 +19,10 @@ function solution(n){
     for (let i = 1; i < answerMax; i++){
       let re = new RegExp(i, 'g');
       let count = (answer.match(re) || []).length;
-  
+      //match 메서드의 반환값이 null일 수 있으므로 null을 반환할 경우에 빈 배열을 사용하도록 한다.
       if(count >= 1){
         result = result + String(i) + String(count);
+        //현재까지 구해진 "count"
       } 
     }
     return result;
